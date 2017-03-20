0ad07166d050ae7405810f20cd31a5c2af07660a
Kari Bancroft <kariabancroft@gmail.com>
2017-03-20 16:37:13 -0700

    Add route, controller action and view file for the 'show'

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,4 +7,20 @@ class BooksController < ApplicationController
       { title: "Kindred", author: "Octavia E. Butler"}
     ]
   end
+
+  def show
+    # puts "<" * 40
+    # puts params[:id]
+    books = [
+      { id: 1, title: "Hidden Figures", author: "Margot Lee Shetterly"},
+      { id: 2, title: "Practical Object-Oriented Design in Ruby", author: "Sandi Metz"},
+      { id: 3, title: "Kindred", author: "Octavia E. Butler"}
+    ]
+    @result_book = nil
+    books.each do |book|
+      if params[:id].to_i == book[:id]
+        @result_book = book
+      end
+    end
+  end
 end
