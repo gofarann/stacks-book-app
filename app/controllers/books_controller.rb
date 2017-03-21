fba075a6242349c9e5c02ad164aeea3629b72f3d
Chris <chris@adadevelopersacademy.org>
2017-03-21 13:55:28 -0700

    Finished Active Record.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,26 +1,20 @@
 class BooksController < ApplicationController
   def index #aka, action
+
+
+    @books = Book.all
+
     # @books = "This is a test"
-    @books = [
-      { title: "Hidden Figures", author: "Margot Lee Shetterly"},
-      { title: "Practical Object-Oriented Design in Ruby", author: "Sandi Metz"},
-      { title: "Kindred", author: "Octavia E. Butler"}
-    ]
+    # @books = [
+    #   { title: "Hidden Figures", author: "Margot Lee Shetterly"},
+    #   { title: "Practical Object-Oriented Design in Ruby", author: "Sandi Metz"},
+    #   { title: "Kindred", author: "Octavia E. Butler"}
+    # ]
   end
 
   def show
-    # puts "<" * 40
-    # puts params[:id]
-    books = [
-      { id: 1, title: "Hidden Figures", author: "Margot Lee Shetterly"},
-      { id: 2, title: "Practical Object-Oriented Design in Ruby", author: "Sandi Metz"},
-      { id: 3, title: "Kindred", author: "Octavia E. Butler"}
-    ]
-    @result_book = nil
-    books.each do |book|
-      if params[:id].to_i == book[:id]
-        @result_book = book
-      end
-    end
+
+    @result_book = Book.find(params[:id])
+
   end
 end
