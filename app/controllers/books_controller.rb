d69836644c78be5c73c82c9671ddcf3197a65001
Kari Bancroft <kariabancroft@gmail.com>
2017-03-22 09:45:59 -0700

    Fix up some formatting of the books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,9 @@
 class BooksController < ApplicationController
-  def index #aka, action
-
-
+  def index
     @books = Book.all
-
-    # @books = "This is a test"
-    # @books = [
-    #   { title: "Hidden Figures", author: "Margot Lee Shetterly"},
-    #   { title: "Practical Object-Oriented Design in Ruby", author: "Sandi Metz"},
-    #   { title: "Kindred", author: "Octavia E. Butler"}
-    # ]
   end
 
   def show
-
     @result_book = Book.find(params[:id])
-
   end
 end
