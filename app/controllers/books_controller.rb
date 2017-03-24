c3d0ff1795cee129e219aa86ef526b320f947e41
Kari Bancroft <kariabancroft@gmail.com>
2017-03-24 12:27:01 -0700

    Add edit functionality, delete and root route. Form partial

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,6 +18,23 @@ class BooksController < ApplicationController
     end
   end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    if book.update(book_params)
+      redirect_to book_path
+    end
+  end
+
+  def destroy
+    Book.destroy(params[:id])
+
+    redirect_to books_path
+  end
+
   private
 
   def book_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "books#index"
 
   get "/books/new", to: "books#new", as: "new_book"
   post "/books", to: "books#create"
 
+  get "/books/:id/edit", to: "books#edit", as: "edit_book"
+  patch "/books/:id", to: "books#update"
+
   get "/books", to: "books#index"
   get "/books/:id", to: "books#show", as: 'book'
+
+  delete "/books/:id", to: "books#destroy", as: "delete_book"
 end
